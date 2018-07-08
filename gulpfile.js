6a01095071b29a4bdd261a4ef3b83ed96ffe9d53
lukelan <luke84320136@gmail.com>
2018-07-08 23:41:36 +0800

    add gulp-plumber, gulp-sass

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,22 @@
-var gulp = require('gulp');
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const plumber = require('gulp-plumber');
 
-gulp.task('copyHTML', function () {
-  return gulp.src('./source/**/*.html')
-    .pipe(gulp.dest('./public/'))
-})
+// gulp.task('copyHTML', function () {
+//   return gulp.src('./source/**/*.html')
+//     .pipe(gulp.dest('./public/'))
+// })
+
+gulp.task('sass', function () {
+  return gulp.src('./source/scss/**/*.scss')
+    .pipe(plumber())
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('./public/css'));
+});
+
+gulp.task('watch', function () {
+  // watch 指定的路徑
+  gulp.watch('./source/scss/**/*.scss', ['sass']);
+});
+
+gulp.task('default', ['sass', 'watch']);
